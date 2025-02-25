ee476fcb57a7b51b050eabdcb234ea7680a8ec12
maorka <maor@0525@gmail.com>
2025-02-25 19:48:19 +0200

    update ui od missions,ad toast for save and delete mission

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,106 @@
 import { FormEvent, useEffect, useState } from 'react';
-//import './App.css';
 import { remult } from "remult";
 import { Task } from "./shared/task";
 import { TaskController } from './shared/TasksControoler';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-  const taskRepo = remult.repo(Task) ;      //repository pattern
+const taskRepo = remult.repo(Task); // repository pattern
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle,setNewTaskTitle]=useState("");//input for new task
-      useEffect(()=>{                   //use effect->happens ones in site reboot
+  const [newTaskTitle, setNewTaskTitle] = useState(""); // input for new task
+
+  useEffect(() => {
     return taskRepo.liveQuery({
-    // where:{
-    //   owner:[remult.user?.id!],
-    // }
-    }).subscribe(info=>setTasks(info.applyChanges));
-  },[]);
+      // where:{
+      //   owner:[remult.user?.id!],
+      // }
+    }).subscribe(info => setTasks(info.applyChanges));
+  }, []);
 
   async function addTask(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();//prevent refreshing 
-    try{
-      const newTask=await taskRepo.insert({title:newTaskTitle});//add new task
-      //setTasks((tasks)=>[...tasks,newTask]);
+    e.preventDefault(); // prevent refreshing
+    try {
+      const newTask = await taskRepo.insert({ title: newTaskTitle }); // add new task
+      // setTasks((tasks) => [...tasks, newTask]);
       setNewTaskTitle("");
+      toast.success("Task saved!");
+    } catch (error: any) {
+      alert(error.message);
     }
-    catch(error:any)
-     {
-      alert(error.message)
-     }
-    }
-    //function to disable/enable all tasks checkpoints ,frontend function
-  // async function setAllCompleted(completed: boolean){
-  //   for (const task of await taskRepo.find()){
-  //     await taskRepo.save({...task,completed})
-  //   } 
-  //  }
+  }
 
-  //1 api call instead of couple like above
-   async function setAllCompleted(completed: boolean){
-         await TaskController.setAllCompleted(completed);//
-    }
-
+  async function setAllCompleted(completed: boolean) {
+    await TaskController.setAllCompleted(completed); //
+  }
 
   return (
     <div>
-      <h1>To Do List</h1> 
+      <h1>To Do List</h1>
       <main>
-        <form onSubmit={e=>addTask(e)}>
-          <input value={newTaskTitle} 
-          onChange={(e)=>setNewTaskTitle(e.target.value)}
-          placeholder='please enter your task'>
+        <form onSubmit={e => addTask(e)}>
+          <input
+            value={newTaskTitle}
+            onChange={(e) => setNewTaskTitle(e.target.value)}
+            placeholder='please enter your task'>
           </input>
           <button>add</button>
         </form>
         {tasks.map((task) => {
-        function setTask(value:Task){
-          setTasks((tasks)=>tasks.map((t)=>(t=== task? value:t)));
-        }
-        async function setCompleted(completed:boolean){
-          await setTask(await taskRepo.save({...task,completed}))
-        }
-          async function deleteTask()  {
-              try{
-              await taskRepo.delete(task);
-              setTasks(tasks=>tasks.filter((t)=>t!==task));
-              } 
-              catch(error:any)
-              {
-                alert(error.message)
-              }         }
-
-          function setTitle(title: string): void {
-            setTask({...task,title});
+          function setTask(value: Task) {
+            setTasks((tasks) => tasks.map((t) => (t === task ? value : t)));
           }
-
-          async function saveTaskUser(){
+          async function setCompleted(completed: boolean) {
+            await setTask(await taskRepo.save({ ...task, completed }));
+          }
+          async function deleteTask() {
             try {
-              setTask(await taskRepo.save(task));
-            } catch (error:any) {
-              alert(error.message)
-              
+              await taskRepo.delete(task);
+              setTasks(tasks => tasks.filter((t) => t !== task));
+              toast.success("Task deleted!");
+            } catch (error: any) {
+              alert(error.message);
             }
           }
 
-          return <div key={task.id}>
-            <input checked={task.completed} type="checkbox" 
-            onChange={(e)=>setCompleted(e.target.checked)}/>
+          function setTitle(title: string): void {
+            setTask({ ...task, title });
+          }
 
-            <input value={task.title} onChange={e=>setTitle(e.target.value
-              )}/>  
-            <button onClick={()=>saveTaskUser()} >Save task</button>
-            <button onClick={()=>deleteTask()}>Delete</button>
-            </div>;
+          async function saveTaskUser() {
+            try {
+              setTask(await taskRepo.save(task));
+              toast.success("Task saved!");
+            } catch (error: any) {
+              alert(error.message);
+            }
+          }
+
+          return (
+            <div key={task.id}>
+              <input
+                checked={task.completed}
+                type="checkbox"
+                onChange={(e) => setCompleted(e.target.checked)}
+              />
+              <textarea
+                value={task.title}
+                onChange={(e) => setTitle(e.target.value)}
+                rows={4} // מספר השורות שניתן לראות
+                cols={50} // מספר העמודות שניתן לראות
+              />
+              <button onClick={() => saveTaskUser()}>Save task</button>
+              <button onClick={() => deleteTask()}>Delete</button>
+            </div>
+          );
         })}
         <div>
-          <button onClick={()=>setAllCompleted(true)}>Set all completed</button>
-          <button onClick={()=>setAllCompleted(false)}>Set all UnCompleted</button>
-
+          <button onClick={() => setAllCompleted(true)}>Set all completed</button>
+          <button onClick={() => setAllCompleted(false)}>Set all UnCompleted</button>
         </div>
       </main>
+      <ToastContainer />
     </div>
   );
 }
